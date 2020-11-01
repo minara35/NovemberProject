e069b054daed77c392c27ce4fa648a341a1de7d3
minara <minara35@gmail.com>
2020-11-01 11:24:23 -0500

    Batman has bat bike

--- src/test/java/Batman.java
+++ src/test/java/Batman.java
@@ -3,6 +3,7 @@ public class Batman {
         System.out.println("I will catch you Joker");
         //yes he will do that
         //Batman has cool batmobile
+        //Batman has bat bike
 
     }
 }
